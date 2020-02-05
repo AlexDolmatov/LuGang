e65e1f78b438ee2e664761d221b17fc0a69f03fa
AlexDolmatov <60095406+AlexDolmatov@users.noreply.github.com>
2020-02-05 14:18:38 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function addColor1(){
+  $("#repass").css("border", "1px solid blue");
+}
+function addColor2(){
+  $("#login").css("border", "1px solid green");
+}
+function addColor3(){
+  $("#pass").css("border", "1px solid red");
+}
