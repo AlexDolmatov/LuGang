1e334099d1e10ad3a8a901613eb2a35e2ef16178
AlexDolmatov <60095406+AlexDolmatov@users.noreply.github.com>
2020-02-05 14:26:25 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function addDraughts(){
+  $("div").filter(".t_kl").slice(0,12).append('<img src="images/tem_shashka.gif">');
+  $("div").filter(".t_kl").slice(20,32).append('<img src="images/sv_shashka.gif">');
+}
